0a0b9681d8ddcb7c08c1c8e83d39a3e2c37ea2cb
anderschristmannCBS <anders@christmann.dk>
2019-11-08 11:53:45 +0100

    Adding a time of booking to the booking system

--- bookingSystem/bookingSystem.js
+++ bookingSystem/bookingSystem.js
@@ -1,6 +1,8 @@
 //Creating an array outside the function so that both functions will have access to it. Before I tried creating it inside one of the functions
 //but was running into issues because of scope.
 var bookingList = [];
+
+//Creating a bookingDateList array to store the date and use it in multiple functions.
 var bookingDateList = [];
 
 //createBooking() function that is run when the button called "book" is clicked.
@@ -14,13 +16,20 @@ function createBooking() {
     bookingList.push(userName);
     bookingList.push(userEmail);
 
+    //Creating a variable called bookingDate that will tell the user at what time they booked.
     var bookingDate = new Date();
+    //Creating a new variable that is the day of the booking using the getDate() method. Using .padStart we add
+    //a 0 to the start of the string so the date will display 0x.
     var dd = String(bookingDate.getDate()).padStart(2, '0');
+    //Creating a new variable that is the month of the booking using the getMonth() method.
     var mm = String(bookingDate.getMonth() + 1).padStart(2, '0');
+    //Creating a new variable that is the year of the booking using the getYear() method.
     var yyyy = bookingDate.getFullYear();
 
+    //Saving the format of the date to the bookingDate variable.
     bookingDate = dd + '/' + mm + '/' + yyyy;
 
+    //Pushing the bookingDate variable to the bookingDateList array.
     bookingDateList.push(bookingDate);
 
     //window.localStorage.setItem("clientName", userName);
@@ -34,14 +43,15 @@ function createBooking() {
 
 //Function that is run when the user clicks the "Show my booking" button.
 function showBookingDetails() {
-    //If statement that checks if the Array exists and if it is empty. We do this because we dont want to try to display the
-    //data of the array if there is nothing in it. Then we would like to display an error message telling the user to create
-    //a booking before checking the details.
-    if (Array.isArray(bookingList) && bookingList.length) {
-        //Creating two new variable that contains the first and second element of the array seperately.
-        var arrayName = bookingList[0];
-        var arrayEmail = bookingList[1];
-        var arrayDate = bookingDateList[0];
+        //If statement that checks if the Array exists and if it is empty. We do this because we dont want to try to display the
+        //data of the array if there is nothing in it. Then we would like to display an error message telling the user to create
+        //a booking before checking the details.
+        if (Array.isArray(bookingList) && bookingList.length) {
+            //Creating two new variable that contains the first and second element of the array separately.
+            var arrayName = bookingList[0];
+            var arrayEmail = bookingList[1];
+            //Creating a new variable arrayDate that store the value of the first element in the bookingDateListArray
+            var arrayDate = bookingDateList[0];
         //var bookingListString = bookingList.toString();
         document.getElementById("bookingDetailsP").innerHTML = "Name: " + arrayName + " E-mail: " + arrayEmail + " Booking Time: " + arrayDate + ".";
         //alert("Client name: " + arrayName + " and Client E-mail: " + arrayEmail);
