b7ad7dd8dd74fc115490fba4e05f96c8dcc8503c
JonasAND <joan19ac@student.cbs.dk>
2019-11-18 14:07:29 +0100

    Jonas

--- simpleLoginWebsite/loginJonas.js
+++ simpleLoginWebsite/loginJonas.js
@@ -16,14 +16,17 @@ var users = [
         password: "axel123"
     }
 ]
-
+//Gets type in info from login page
 function getInfo() {
     var username = document.getElementById("username").value
     var password = document.getElementById("password").value
-
+//Checks login info against saved users
     for (i = 0; i < users.length; i++) {
         if(username == users[i].username && password == users[i].password) {
             console.log(username + " is now logged in! :D")
+        } else {
+            console.log("IM TIRED OF THESE MOTHERFUCKING FAKES ON MY MOTHERFUCKING WEBSITE")
         }
     }
 }
+//https://www.youtube.com/watch?v=UAu7cMuu0BI&t=9s
